bd26ae37cfdea2fadc205fef9064a14d911855f9
danielp <you@example.com>
2023-06-30 14:33:18 +0200

    moves watcher's classes to instance-like approach

--- watcher.py
+++ watcher.py
@@ -6,53 +6,44 @@ from typing import Any, Optional
 class WatcherBase(ABC):
     """ Abstract class for different type of Watchers. """
 
-    @classmethod
     @abstractmethod
-    def watch(cls, **spot_settings) -> Any:
+    def watch(self, **spot_settings) -> Any:
         pass
 
-    @classmethod
     @abstractmethod
-    def __prepare_watching_spot_settings(cls, *args, **kwargs) -> Optional[dict]:
+    def __prepare_watching_spot_settings(self, *args, **kwargs) -> Optional[dict]:
         pass
 
-    @classmethod
     @abstractmethod
-    def __parse_watching_findings(cls, *args, **kwargs) -> Any:
+    def __parse_watching_findings(self, *args, **kwargs) -> Any:
         pass
 
 
 class IpSocksWatcher(WatcherBase):
     """ Adopts / utilizes psutil api for providing network sockets information. """
 
-    @classmethod
     @abstractmethod
-    def watch(cls, **spot_settings) -> Any:
+    def watch(self, **spot_settings) -> Any:
         pass
 
-    @classmethod
-    def __prepare_watching_spot_settings(cls, *args, **kwargs) -> None:
+    def __prepare_watching_spot_settings(self, *args, **kwargs) -> None:
         pass
 
-    @classmethod
-    def __parse_watching_findings(cls, *args, **kwargs) -> None:
+    def __parse_watching_findings(self, *args, **kwargs) -> None:
         pass
 
 
 class UnixSocksWatcher(WatcherBase):
     """ Adopts / utilizes psutil api for providing Unix sockets information. """
 
-    @classmethod
     @abstractmethod
-    def watch(cls, **spot_settings) -> Any:
+    def watch(self, **spot_settings) -> Any:
         pass
 
-    @classmethod
     @abstractmethod
-    def __prepare_watching_spot_settings(cls, *args, **kwargs) -> Optional[dict]:
+    def __prepare_watching_spot_settings(self, *args, **kwargs) -> Optional[dict]:
         pass
 
-    @classmethod
     @abstractmethod
-    def __parse_watching_findings(cls, *args, **kwargs) -> Any:
+    def __parse_watching_findings(self, *args, **kwargs) -> Any:
         pass
