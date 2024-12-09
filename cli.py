cbaf9d6370ddc7aba5341ca850320e2b57a4366b
Daniel <daniel.palacz@pyx.solutions>
2024-12-09 17:20:55 +0100

    implements basic cli command

---
+++ cli.py
@@ -0,0 +1,47 @@
+import click
+
+from watcher import Ip4ConnectionAnalyzer, BasicReporter, IpConnectionWatcher, SupervisorManager
+
+
+class Config:
+    """Configuration class will be used across click module context for handling configuration details"""
+
+    def __init__(self):
+        self.version = "0.1"
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+@click.group()
+@pass_config
+def cli(config):
+    """Hello in the app!"""
+    pass
+
+
+@cli.command()
+@pass_config
+@click.option('--report_type', default="Console", help='Type of report.')
+def ip4_connections_check(config, report_type) -> None:
+    """ Command runs IP4 connections checks.
+
+    \b
+    report_type:
+      - by default it is Console report"""
+
+    report_types = ["Console"]
+
+    if report_type == "Console":
+        basic_reporter = BasicReporter()
+    else:
+        raise ValueError("report type has to be one of:", report_types)
+
+    ip_analyzer = Ip4ConnectionAnalyzer()
+    ip_watcher = IpConnectionWatcher(ip_kind="IP4", transport_kind="TCP")
+    supervisor = SupervisorManager(analyzer=ip_analyzer, reporter=basic_reporter, watcher=ip_watcher)
+    supervisor.report()
+
+
+if __name__ == "__main__":
+    cli()
